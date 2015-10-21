2dcc8cc135fa064c9129924a14dda7ec40f3034c
Kel Cecil <kelcecil@praisechaos.com>
2015-10-21 17:03:31 -0400

    Add idea project files to gitignore

---
+++ src/main/java/org/kelcecil/globalgiving/action/GetThemes.java
@@ -0,0 +1,7 @@
+package org.kelcecil.globalgiving.action;
+
+/**
+ * Created by kelcecil on 10/21/15.
+ */
+public class GetThemes {
+}
---
+++ src/main/java/org/kelcecil/globalgiving/model/api/GetThemesResponse.java
@@ -0,0 +1,7 @@
+package org.kelcecil.globalgiving.model.api;
+
+/**
+ * Created by kelcecil on 10/21/15.
+ */
+public class GetThemesResponse {
+}
