e8c5240fc26869294e059826d5dc3cebd3a7a384
Kel Cecil <kelcecil@praisechaos.com>
2015-10-21 07:31:53 -0400

    Add Theme model

---
+++ src/main/java/org/kelcecil/globalgiving/GlobalGivingClient.java
@@ -0,0 +1,7 @@
+package org.kelcecil.globalgiving;
+
+/**
+ * Created by kelcecil on 10/21/15.
+ */
+public class GlobalGivingClient {
+}
---
+++ src/main/java/org/kelcecil/globalgiving/model/Theme.java
@@ -0,0 +1,26 @@
+package org.kelcecil.globalgiving.model;
+
+/**
+ * Created by kelcecil on 10/21/15.
+ */
+public class Theme {
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
